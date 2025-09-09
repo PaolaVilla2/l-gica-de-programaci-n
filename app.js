da9bb11e03b3904f74a55dedcfeea16ae1c0558d
PaolaVilla2 <164100806+PaolaVilla2@users.noreply.github.com>
2025-09-09 15:30:14 -0600

    agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//variables
+let amigos = [];
+
+function agregarAmigo() {
+  // Capturar el valor del campo de entrada
+  let input = document.getElementById("amigo");
+
+  // Validar que el campo no este vacío
+  if (nombre === "") {
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
+
+  // Agregar al array
+  amigos.push(nombre);
+
+  // Mostrar en la lista de amigos
+  mostrarAmigos();
+
+  // Limpiar el campo de entrada
+  input.value = "";
+}
+
+function mostrarAmigos() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = ""; // Limpiar lista antes de actualizar
+
+  amigos.forEach(function(amigo) {
+    let li = document.createElement("li");
+    li.textContent = amigo;
+    lista.appendChild(li);
+  });
+}
